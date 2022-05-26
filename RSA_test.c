b6c0f947e95e82f3b155f807ab7d88bab29157d0
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-05-26 08:54:03 -0700

    Minor refactoring in test framework

--- RSA_test.c
+++ RSA_test.c
@@ -8,21 +8,21 @@ int assert(int condition, char* test_name);
 
 int trivial_encrypt() {
     int failed_tests = 0;
-    failed_tests = failed_tests + assert(encrypt(123) == 855, "trivial encrypt from slides");
+    failed_tests += assert(encrypt(123) == 855, "trivial encrypt from slides");
     return failed_tests;
 }
 
 int trivial_decrypt() {
     int failed_tests = 0;
-    failed_tests = failed_tests + assert(decrypt(855) == 123, "trivial decrypt from slides");
+    failed_tests += assert(decrypt(855) == 123, "trivial decrypt from slides");
     return failed_tests;
 }
 
 void test() {
     printf("Starting tests for RSA\n\n");
     int failed_tests = 0;
-    failed_tests = failed_tests + trivial_encrypt();
-    failed_tests = failed_tests + trivial_decrypt();
+    failed_tests += trivial_encrypt();
+    failed_tests += trivial_decrypt();
     printf("Tests completed for RSA with %d failed tests\n\n", failed_tests);
 }
 
