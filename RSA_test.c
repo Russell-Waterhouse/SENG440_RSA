afbc473f3afbc3d0ab66b168dffb06e3e5dfd79f
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-05-26 09:13:21 -0700

    add property testing

--- RSA_test.c
+++ RSA_test.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include "RSA.h"
+#include <stdlib.h>
 
 int assert(int condition, char* test_name);
 
@@ -18,11 +19,33 @@ int trivial_decrypt() {
     return failed_tests;
 }
 
+int property_test() {
+    int failed_tests = 0;
+    int i = 0;
+    unsigned int input;
+    unsigned int cyphertext;
+    unsigned int decyphered_text;
+    for(i=0; i<=100; i++) {
+        input = rand();
+//        This is just there because the current implementation has pq=3233
+//        input cannot be greater than 3233
+        input = input % 3233;
+        cyphertext = encrypt(input);
+        decyphered_text = decrypt(cyphertext);
+        failed_tests += assert(input==decyphered_text, "Property testing encrypt/decrypt");
+        if (input != decyphered_text){
+            printf("Property testing failed with input %u; cyphertext was %u, decyphered_text was %u\n", input, cyphertext, decyphered_text);
+        }
+    }
+    return failed_tests;
+}
+
 void test() {
     printf("Starting tests for RSA\n\n");
     int failed_tests = 0;
     failed_tests += trivial_encrypt();
     failed_tests += trivial_decrypt();
+    failed_tests += property_test();
     printf("Tests completed for RSA with %d failed tests\n\n", failed_tests);
 }
 
