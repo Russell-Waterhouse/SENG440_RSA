a995e33dacc08eaeaf263238a793be72bc387a4b
Dave Newcombe <dave.newcombe@mazlite.com>
2022-07-26 10:11:58 -0700

    Included original unoptimized functions; performance now compares initial and optimized

--- RSA.h
+++ RSA.h
@@ -6,5 +6,7 @@
 
 uint64_t encrypt(uint64_t input);
 uint64_t decrypt(uint64_t input);
+uint64_t initial_encrypt(uint64_t input);
+uint64_t initial_decrypt(uint64_t input);
 
 #endif
--- RSA_test.c
+++ RSA_test.c
@@ -10,6 +10,18 @@
 int assert(int condition, char* test_name);
 void measure_performance();
 
+int initial_trivial_encrypt() {
+    int failed_tests = 0;
+    failed_tests += assert(initial_encrypt(123) == 855, "trivial encrypt from slides");
+    return failed_tests;
+}
+
+int initial_trivial_decrypt() {
+    int failed_tests = 0;
+    failed_tests += assert(initial_decrypt(855) == 123, "trivial decrypt from slides");
+    return failed_tests;
+}
+
 int trivial_encrypt() {
     int failed_tests = 0;
     failed_tests += assert(encrypt(123) == 855, "trivial encrypt from slides");
@@ -48,6 +60,8 @@ void test(int performance_flag) {
 
     int failed_tests = 0;
 
+    failed_tests += initial_trivial_encrypt();
+    failed_tests += initial_trivial_decrypt();
     failed_tests += trivial_encrypt();
     failed_tests += trivial_decrypt();
     failed_tests += property_test();
@@ -59,30 +73,6 @@ void test(int performance_flag) {
         measure_performance();
 }
 
-void measure_performance() {
-    uint64_t start = 0;
-    uint64_t end = 0;
-    uint64_t total = 0;
-    uint64_t average = 0;
-    uint64_t num_trials = 10000;
-
-    printf("+ Starting performance for RSA\n\n");
-    
-    start = clock();
-    for ( int i=0; i < num_trials; i++ ) {
-        trivial_encrypt();
-        trivial_decrypt();
-    }
-    end = clock(); 
-
-    total = end - start;
-    average = total / num_trials;
-
-    printf("Results:\n");
-    printf("\tNumber of trials = %ld\n", num_trials);
-    printf("\tAverage Ticks: %ld (microseconds)\n\n", average);
-}
-
 int assert(int condition, char* test_name){
     if (condition) {
         return 0;
@@ -91,4 +81,47 @@ int assert(int condition, char* test_name){
         printf("TEST FAILED: %s\n", test_name);
         return 1;
     }
+}
+
+void measure_performance() {
+    uint64_t start = 0;
+    uint64_t end = 0;
+    uint64_t total = 0;
+    uint64_t result = 0;
+    uint64_t average1 = 0;
+    uint64_t average2 = 0;
+    uint64_t num_trials = 100000;
+
+    printf("+ Starting performance evaluation for RSA\n\n");
+    printf("Pure Software Implementation:\n");
+
+    start = clock();
+    for ( int i=0; i < num_trials; i++ ) {
+        initial_trivial_encrypt();
+        initial_trivial_decrypt();
+    }
+    end = clock(); 
+    total = end - start;
+    average1 = total / num_trials;
+
+    printf("\tAverage Ticks: %ld cycles\n\n", average1);
+    printf("Optimized Implementation:\n");
+
+    start = clock();
+    for ( int i=0; i < num_trials; i++ ) {
+        trivial_encrypt();
+        trivial_decrypt();
+    }
+    end = clock(); 
+    total = end - start;
+    average2 = total / num_trials;
+
+    result = average2- average2;
+    if ( result < 0 )
+        result = 0;
+
+    printf("\tAverage Ticks: %ld cycles\n\n", average2);
+    printf("Results:\n");
+    printf("\tNumber of trials = %ld\n", num_trials);
+    printf("\tImproved by %ld cycles\n\n", (result));
 }
